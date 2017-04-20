18b31363f0837cbefb9b5892aede19e4a01fd026
Drew Van Hise <dvanhise@gmail.com>
2017-04-20 04:07:28 -0400

    laptop changes

--- 10958.py
+++ 10958.py
@@ -7,22 +7,33 @@ EVAL_TIMEOUT = 2
 
 
 def main():
+    logDict = {
+        'int': 0,
+        'float': 0,
+        'timeout': 0,
+        'other': 0
+    }
+
     seq = Sequence(DIGIT_SEQUENCE)
     for expression in seq:
         try:
             result = evalWrapper(expression)
-            if type(result) is float and result.is_integer():
+            if (type(result) is float and result.is_integer()) or type(result) is int:
                 result = int(result)
-            if result == MAGIC_NUMBER:
-                print('Result: {d}, Sequence: {s}'.format(result, expression))
+                if (result > 10 and result < 50) or result == MAGIC_NUMBER:
+                    print('Result: {}, Sequence: {}'.format(result, expression))
+                logDict['int'] += 1
+            else:
+                logDict['float'] += 1
         except TimeoutError:
-            print('Timeout: ' + expression)
+            logDict['timeout'] += 1
         except:
-            print('Other failure: ' + expression)
-            raise
+            logDict['other'] += 1
+
+        print(logDict)
 
 
-@timeout(EVAL_TIMEOUT, timeout_exception=StopIteration)
+@timeout(EVAL_TIMEOUT, timeout_exception=TimeoutError)
 def evalWrapper(exp):
     return eval(exp)
 
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Solving the 10958 problem
 https://www.youtube.com/watch?v=-ruC5A9EzzE
 
-Will only run in a Unix environment because of the timeout_decorator library.
+Will only run Unix with Python 3.x
